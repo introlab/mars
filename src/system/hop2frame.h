0ce19e460db776692d1be3245d68500cff4ed16f
FrancoisGrondin <francois.grondin2@usherbrooke.ca>
2016-10-25 16:22:27 -0400

    Documentation updated

--- src/system/hop2frame.h
+++ src/system/hop2frame.h
@@ -5,7 +5,27 @@
 
 	#include <stdlib.h>
 
-	//! \file hop2frame.h
+	/**
+	* \file 	hop2frame.h
+	* \author 	François Grondin <francois.grondin2@usherbrooke.ca>
+	* \version  1.0
+	* \date 	2016-10-25
+	* \copyright
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+	* 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*
+	*/
 
 	//! A structure that holds all the fields to convert hops to frames. 
 	typedef struct hop2frame_obj {
--- src/utils/indexing.h
+++ src/utils/indexing.h
@@ -1,7 +1,27 @@
 #ifndef __MARS_UTILS_INDEXING
 #define __MARS_UTILS_INDEXING
 
-	//! \file indexing.h
+	/**
+	* \file 	indexing.h
+	* \author 	François Grondin <francois.grondin2@usherbrooke.ca>
+	* \version  1.0
+	* \date 	2016-10-25
+	* \copyright
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+	* 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*
+	*/
 
 	/** Wrap the index withing the positive range [0,N-1]
 		\param 		index   	The index that is a signed integer
