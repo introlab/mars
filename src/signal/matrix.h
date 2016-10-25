9ac5eb0e4a132fb3ce9e8ccbf87e4551f3b38888
FrancoisGrondin <francois.grondin2@usherbrooke.ca>
2016-10-25 17:10:41 -0400

    Tabs fixed.

--- src/signal/matrix.h
+++ src/signal/matrix.h
@@ -1,196 +1,196 @@
 #ifndef __MARS_SIGNAL_MATRIX
 #define __MARS_SIGNAL_MATRIX
 
-	#include <stdlib.h>
+    #include <stdlib.h>
 
-	/**
-	* \file 	matrix.h
-	* \author 	François Grondin <francois.grondin2@usherbrooke.ca>
-	* \version  1.0
-	* \date 	2016-10-25
-	* \copyright
-	*
-	* This program is free software: you can redistribute it and/or modify
-	* it under the terms of the GNU General Public License as published by
-	* the Free Software Foundation, either version 3 of the License, or
-	* (at your option) any later version.
-	*
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	* GNU General Public License for more details.
-	* 
-	* You should have received a copy of the GNU General Public License
-	* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	*
-	*/
+    /**
+    * \file     matrix.h
+    * \author   François Grondin <francois.grondin2@usherbrooke.ca>
+    * \version  1.0
+    * \date     2016-10-25
+    * \copyright
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    */
 
-	//! A structure that holds all the fields of a matrix with signed char elements. 
-	typedef struct matrix_signedchar {
+    //! A structure that holds all the fields of a matrix with signed char elements. 
+    typedef struct matrix_signedchar {
 
-		signed char ** array;		///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        signed char ** array;       ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_signedchar;
+    } matrix_signedchar;
 
-	//! A structure that holds all the fields of a matrix with unsigned char elements. 
-	typedef struct matrix_unsignedchar {
+    //! A structure that holds all the fields of a matrix with unsigned char elements. 
+    typedef struct matrix_unsignedchar {
 
-		unsigned char ** array;		///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        unsigned char ** array;     ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_unsignedchar;
+    } matrix_unsignedchar;
 
-	//! A structure that holds all the fields of a matrix with signed short elements. 
-	typedef struct matrix_signedshort {
+    //! A structure that holds all the fields of a matrix with signed short elements. 
+    typedef struct matrix_signedshort {
 
-		signed short ** array;		///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        signed short ** array;      ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_signedshort;
+    } matrix_signedshort;
 
-	//! A structure that holds all the fields of a matrix with unsigned short elements. 
-	typedef struct matrix_unsignedshort {
+    //! A structure that holds all the fields of a matrix with unsigned short elements. 
+    typedef struct matrix_unsignedshort {
 
-		unsigned short ** array;	///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        unsigned short ** array;    ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_unsignedshort;
+    } matrix_unsignedshort;
 
-	//! A structure that holds all the fields of a matrix with signed int elements. 
-	typedef struct matrix_signedint {
+    //! A structure that holds all the fields of a matrix with signed int elements. 
+    typedef struct matrix_signedint {
 
-		signed int ** array;		///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        signed int ** array;        ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_signedint;
+    } matrix_signedint;
 
-	//! A structure that holds all the fields of a matrix with unsigned int elements. 
-	typedef struct matrix_unsignedint {
+    //! A structure that holds all the fields of a matrix with unsigned int elements. 
+    typedef struct matrix_unsignedint {
 
-		unsigned int ** array;		///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        unsigned int ** array;      ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_unsignedint;
+    } matrix_unsignedint;
 
-	//! A structure that holds all the fields of a matrix with float elements. 
-	typedef struct matrix_float {
+    //! A structure that holds all the fields of a matrix with float elements. 
+    typedef struct matrix_float {
 
-		float ** array;				///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        float ** array;             ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_float;
+    } matrix_float;
 
-	//! A structure that holds all the fields of a matrix with double elements. 
-	typedef struct matrix_double {
+    //! A structure that holds all the fields of a matrix with double elements. 
+    typedef struct matrix_double {
 
-		double ** array;			///< Pointer to the array of elements.		 
-		unsigned int nRows;			///< Number of rows.
-		unsigned int nCols;			///< Number of columns.
+        double ** array;            ///< Pointer to the array of elements.		 
+        unsigned int nRows;         ///< Number of rows.
+        unsigned int nCols;         ///< Number of columns.
 
-	} matrix_double;
+    } matrix_double;
 
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/
-	matrix_signedchar * matrix_signedchar_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */
+    matrix_signedchar * matrix_signedchar_malloc(const unsigned int nRows, const unsigned int nCols);
 
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/
-	matrix_unsignedchar * matrix_unsignedchar_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */
+    matrix_unsignedchar * matrix_unsignedchar_malloc(const unsigned int nRows, const unsigned int nCols);
+
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */
+    matrix_signedshort * matrix_signedshort_malloc(const unsigned int nRows, const unsigned int nCols);
+
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */	
+    matrix_unsignedshort * matrix_unsignedshort_malloc(const unsigned int nRows, const unsigned int nCols);
+
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */	
+    matrix_signedint * matrix_signedint_malloc(const unsigned int nRows, const unsigned int nCols);
 	
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/
-	matrix_signedshort * matrix_signedshort_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */
+    matrix_unsignedint * matrix_unsignedint_malloc(const unsigned int nRows, const unsigned int nCols);
+
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */
+    matrix_float * matrix_float_malloc(const unsigned int nRows, const unsigned int nCols);
+
+    /** Constructor of the vector object.	
+        \param      nRows       Number of rows.
+        \param      nCols       Number of columns.
+        \return                 Pointer to the instantiated object.
+    */
+    matrix_double * matrix_double_malloc(const unsigned int nRows, const unsigned int nCols);
+
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_signedchar_free(matrix_signedchar * obj);
 	
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/	
-	matrix_unsignedshort * matrix_unsignedshort_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_unsignedchar_free(matrix_unsignedchar * obj);
 
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/	
-	matrix_signedint * matrix_signedint_malloc(const unsigned int nRows, const unsigned int nCols);
-	
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/
-	matrix_unsignedint * matrix_unsignedint_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_signedshort_free(matrix_signedshort * obj);
 
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/
-	matrix_float * matrix_float_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_unsignedshort_free(matrix_unsignedshort * obj);
 
-	/** Constructor of the vector object.	
-		\param		nRows		Number of rows.
-		\param		nCols		Number of columns.
-		\return					Pointer to the instantiated object.
-	*/
-	matrix_double * matrix_double_malloc(const unsigned int nRows, const unsigned int nCols);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_signedint_free(matrix_signedint * obj);
 
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_signedchar_free(matrix_signedchar * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_unsignedchar_free(matrix_unsignedchar * obj);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_unsignedint_free(matrix_unsignedint * obj);
 
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_signedshort_free(matrix_signedshort * obj);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_float_free(matrix_float * obj);
 
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_unsignedshort_free(matrix_unsignedshort * obj);
-
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_signedint_free(matrix_signedint * obj);
-
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_unsignedint_free(matrix_unsignedint * obj);
-
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_float_free(matrix_float * obj);
-
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void matrix_double_free(matrix_double * obj);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void matrix_double_free(matrix_double * obj);
 
 #endif
--- src/signal/vector.h
+++ src/signal/vector.h
@@ -1,180 +1,180 @@
 #ifndef __MARS_SIGNAL_VECTOR
 #define __MARS_SIGNAL_VECTOR
 
-	#include <stdlib.h>
+    #include <stdlib.h>
 
-	/**
-	* \file		vector.h
-	* \author	François Grondin <francois.grondin2@usherbrooke.ca>
-	* \version	1.0
-	* \date		2016-10-25
-	* \copyright
-	*
-	* This program is free software: you can redistribute it and/or modify
-	* it under the terms of the GNU General Public License as published by
-	* the Free Software Foundation, either version 3 of the License, or
-	* (at your option) any later version.
-	*
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	* GNU General Public License for more details.
-	* 
-	* You should have received a copy of the GNU General Public License
-	* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	*
-	*/
+    /**
+    * \file     vector.h
+    * \author   François Grondin <francois.grondin2@usherbrooke.ca>
+    * \version  1.0
+    * \date     2016-10-25
+    * \copyright
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    */
 
-	//! A structure that holds all the fields of a vector with signed char elements. 
-	typedef struct vector_signedchar {
+    //! A structure that holds all the fields of a vector with signed char elements. 
+    typedef struct vector_signedchar {
 
-		signed char * array;		///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        signed char * array;        ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_signedchar;
+    } vector_signedchar;
 
-	//! A structure that holds all the fields of a vector with unsigned char elements. 
-	typedef struct vector_unsignedchar {
+    //! A structure that holds all the fields of a vector with unsigned char elements. 
+    typedef struct vector_unsignedchar {
 
-		unsigned char * array;		///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        unsigned char * array;      ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_unsignedchar;
+    } vector_unsignedchar;
 
-	//! A structure that holds all the fields of a vector with signed short elements. 
-	typedef struct vector_signedshort {
+    //! A structure that holds all the fields of a vector with signed short elements. 
+    typedef struct vector_signedshort {
 
-		signed short * array;		///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        signed short * array;       ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_signedshort;
+    } vector_signedshort;
 
-	//! A structure that holds all the fields of a vector with unsigned short elements. 
-	typedef struct vector_unsignedshort {
+    //! A structure that holds all the fields of a vector with unsigned short elements. 
+    typedef struct vector_unsignedshort {
 
-		unsigned short * array;		///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        unsigned short * array;     ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_unsignedshort;
+    } vector_unsignedshort;
 
-	//! A structure that holds all the fields of a vector with signed int elements. 
-	typedef struct vector_signedint {
+    //! A structure that holds all the fields of a vector with signed int elements. 
+    typedef struct vector_signedint {
 
-		signed int * array;			///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        signed int * array;         ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_signedint;
+    } vector_signedint;
 
-	//! A structure that holds all the fields of a vector with unsigned int elements. 
-	typedef struct vector_unsignedint {
+    //! A structure that holds all the fields of a vector with unsigned int elements. 
+    typedef struct vector_unsignedint {
 
-		unsigned int * array;		///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        unsigned int * array;       ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_unsignedint;
+    } vector_unsignedint;
 
-	//! A structure that holds all the fields of a vector with float elements. 
-	typedef struct vector_float {
+    //! A structure that holds all the fields of a vector with float elements. 
+    typedef struct vector_float {
 
-		float * array;				///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        float * array;              ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_float;
+    } vector_float;
 
-	//! A structure that holds all the fields of a vector with double elements. 
-	typedef struct vector_double {
+    //! A structure that holds all the fields of a vector with double elements. 
+    typedef struct vector_double {
 
-		double * array;				///< Pointer to the array of elements.		 
-		unsigned int nElements;		///< Number of elements.
+        double * array;             ///< Pointer to the array of elements.		 
+        unsigned int nElements;     ///< Number of elements.
 
-	} vector_double;
+    } vector_double;
 
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/
-	vector_signedchar * vector_signedchar_malloc(const unsigned int nElements);
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */
+    vector_signedchar * vector_signedchar_malloc(const unsigned int nElements);
 
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/
-	vector_unsignedchar * vector_unsignedchar_malloc(const unsigned int nElements);
-	
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/
-	vector_signedshort * vector_signedshort_malloc(const unsigned int nElements);
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */
+    vector_unsignedchar * vector_unsignedchar_malloc(const unsigned int nElements);
+    
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */
+    vector_signedshort * vector_signedshort_malloc(const unsigned int nElements);
 
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */	
+    vector_unsignedshort * vector_unsignedshort_malloc(const unsigned int nElements);
+
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */	
+    vector_signedint * vector_signedint_malloc(const unsigned int nElements);
+
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */	
+    vector_unsignedint * vector_unsignedint_malloc(const unsigned int nElements);
+
+    /** Constructor of the vector object.	
+        \param     nElements    Number of elements.
+        \return                 Pointer to the instantiated object.
+    */	
+    vector_float * vector_float_malloc(const unsigned int nElements);
+
+    /** Constructor of the vector object.	
+        \param      nElements   Number of elements.
+        \return                 Pointer to the instantiated object.
+    */	
+    vector_double * vector_double_malloc(const unsigned int nElements);
+
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_signedchar_free(vector_signedchar * obj);
+
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_unsignedchar_free(vector_unsignedchar * obj);
+
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_signedshort_free(vector_signedshort * obj);
+
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
 	*/	
-	vector_unsignedshort * vector_unsignedshort_malloc(const unsigned int nElements);
-	
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/	
-	vector_signedint * vector_signedint_malloc(const unsigned int nElements);
+    void vector_unsignedshort_free(vector_unsignedshort * obj);
 
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/	
-	vector_unsignedint * vector_unsignedint_malloc(const unsigned int nElements);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_signedint_free(vector_signedint * obj);
 
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/	
-	vector_float * vector_float_malloc(const unsigned int nElements);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_unsignedint_free(vector_unsignedint * obj);
 
-	/** Constructor of the vector object.	
-		\param		nElements	Number of elements.
-		\return					Pointer to the instantiated object.
-	*/	
-	vector_double * vector_double_malloc(const unsigned int nElements);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_float_free(vector_float * obj);
 
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void vector_signedchar_free(vector_signedchar * obj);
-
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void vector_unsignedchar_free(vector_unsignedchar * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void vector_signedshort_free(vector_signedshort * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/	
-	void vector_unsignedshort_free(vector_unsignedshort * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj 		Pointer to the instantiated object.
-	*/
-	void vector_signedint_free(vector_signedint * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void vector_unsignedint_free(vector_unsignedint * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void vector_float_free(vector_float * obj);
-	
-	/** Destructor of the vector object.
-		\param		obj			Pointer to the instantiated object.
-	*/
-	void vector_double_free(vector_double * obj);
+    /** Destructor of the vector object.
+        \param      obj         Pointer to the instantiated object.
+    */
+    void vector_double_free(vector_double * obj);
 
 #endif
