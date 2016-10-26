9a5bf7d572284e90e5fac6a30adc9dd99d193fa9
FrancoisGrondin <francois.grondin2@usherbrooke.ca>
2016-10-26 13:38:50 -0400

    Tabs fixed.

--- src/signal/matrix.c
+++ src/signal/matrix.c
@@ -1,258 +1,258 @@
-	
-	#include "matrix.h"
+    
+    #include "matrix.h"
 
-	matrix_signedchar * matrix_signedchar_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_signedchar * matrix_signedchar_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_signedchar * obj;
-		unsigned int iRow;
+        matrix_signedchar * obj;
+        unsigned int iRow;
 
-		obj = (matrix_signedchar *) malloc(sizeof(matrix_signedchar));
+        obj = (matrix_signedchar *) malloc(sizeof(matrix_signedchar));
 
-		obj->array = (signed char **) malloc(sizeof(signed char *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (signed char *) malloc(sizeof(signed char) * nCols);
-		}
+        obj->array = (signed char **) malloc(sizeof(signed char *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (signed char *) malloc(sizeof(signed char) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_unsignedchar * matrix_unsignedchar_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_unsignedchar * matrix_unsignedchar_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_unsignedchar * obj;
-		unsigned int iRow;
+        matrix_unsignedchar * obj;
+        unsigned int iRow;
 
-		obj = (matrix_unsignedchar *) malloc(sizeof(matrix_unsignedchar));
+        obj = (matrix_unsignedchar *) malloc(sizeof(matrix_unsignedchar));
 
-		obj->array = (unsigned char **) malloc(sizeof(unsigned char *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (unsigned char *) malloc(sizeof(unsigned char) * nCols);
-		}
+        obj->array = (unsigned char **) malloc(sizeof(unsigned char *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (unsigned char *) malloc(sizeof(unsigned char) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_signedshort * matrix_signedshort_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_signedshort * matrix_signedshort_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_signedshort * obj;
-		unsigned int iRow;
+        matrix_signedshort * obj;
+        unsigned int iRow;
 
-		obj = (matrix_signedshort *) malloc(sizeof(matrix_signedshort));
+        obj = (matrix_signedshort *) malloc(sizeof(matrix_signedshort));
 
-		obj->array = (signed short **) malloc(sizeof(signed short *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (signed short *) malloc(sizeof(signed short) * nCols);
-		}
+        obj->array = (signed short **) malloc(sizeof(signed short *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (signed short *) malloc(sizeof(signed short) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_unsignedshort * matrix_unsignedshort_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_unsignedshort * matrix_unsignedshort_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_unsignedshort * obj;
-		unsigned int iRow;
+        matrix_unsignedshort * obj;
+        unsigned int iRow;
 
-		obj = (matrix_unsignedshort *) malloc(sizeof(matrix_unsignedshort));
+        obj = (matrix_unsignedshort *) malloc(sizeof(matrix_unsignedshort));
 
-		obj->array = (unsigned short **) malloc(sizeof(unsigned short *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (unsigned short *) malloc(sizeof(unsigned short) * nCols);
-		}
+        obj->array = (unsigned short **) malloc(sizeof(unsigned short *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (unsigned short *) malloc(sizeof(unsigned short) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_signedint * matrix_signedint_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_signedint * matrix_signedint_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_signedint * obj;
-		unsigned int iRow;
+        matrix_signedint * obj;
+        unsigned int iRow;
 
-		obj = (matrix_signedint *) malloc(sizeof(matrix_signedint));
+        obj = (matrix_signedint *) malloc(sizeof(matrix_signedint));
 
-		obj->array = (signed int **) malloc(sizeof(signed int *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (signed int *) malloc(sizeof(signed int) * nCols);
-		}
+        obj->array = (signed int **) malloc(sizeof(signed int *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (signed int *) malloc(sizeof(signed int) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_unsignedint * matrix_unsignedint_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_unsignedint * matrix_unsignedint_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_unsignedint * obj;
-		unsigned int iRow;
+        matrix_unsignedint * obj;
+        unsigned int iRow;
 
-		obj = (matrix_unsignedint *) malloc(sizeof(matrix_unsignedint));
+        obj = (matrix_unsignedint *) malloc(sizeof(matrix_unsignedint));
 
-		obj->array = (unsigned int **) malloc(sizeof(unsigned int *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (unsigned int *) malloc(sizeof(unsigned int) * nCols);
-		}
+        obj->array = (unsigned int **) malloc(sizeof(unsigned int *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (unsigned int *) malloc(sizeof(unsigned int) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_float * matrix_float_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_float * matrix_float_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_float * obj;
-		unsigned int iRow;
+        matrix_float * obj;
+        unsigned int iRow;
 
-		obj = (matrix_float *) malloc(sizeof(matrix_float));
+        obj = (matrix_float *) malloc(sizeof(matrix_float));
 
-		obj->array = (float **) malloc(sizeof(float *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (float *) malloc(sizeof(float) * nCols);
-		}
+        obj->array = (float **) malloc(sizeof(float *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (float *) malloc(sizeof(float) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	matrix_double * matrix_double_malloc(const unsigned int nRows, const unsigned int nCols) {
+    matrix_double * matrix_double_malloc(const unsigned int nRows, const unsigned int nCols) {
 
-		matrix_double * obj;
-		unsigned int iRow;
+        matrix_double * obj;
+        unsigned int iRow;
 
-		obj = (matrix_double *) malloc(sizeof(matrix_double));
+        obj = (matrix_double *) malloc(sizeof(matrix_double));
 
-		obj->array = (double **) malloc(sizeof(double *) * nRows);
-		
-		for (iRow = 0; iRow < nRows; iRow++) {
-			obj->array[iRow] = (double *) malloc(sizeof(double) * nCols);
-		}
+        obj->array = (double **) malloc(sizeof(double *) * nRows);
+        
+        for (iRow = 0; iRow < nRows; iRow++) {
+            obj->array[iRow] = (double *) malloc(sizeof(double) * nCols);
+        }
 
-		obj->nRows = nRows;
-		obj->nCols = nCols;
+        obj->nRows = nRows;
+        obj->nCols = nCols;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	void matrix_signedchar_free(matrix_signedchar * obj) {
+    void matrix_signedchar_free(matrix_signedchar * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_unsignedchar_free(matrix_unsignedchar * obj) {
+    void matrix_unsignedchar_free(matrix_unsignedchar * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_signedshort_free(matrix_signedshort * obj) {
+    void matrix_signedshort_free(matrix_signedshort * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_unsignedshort_free(matrix_unsignedshort * obj) {
+    void matrix_unsignedshort_free(matrix_unsignedshort * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_signedint_free(matrix_signedint * obj) {
+    void matrix_signedint_free(matrix_signedint * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_unsignedint_free(matrix_unsignedint * obj) {
+    void matrix_unsignedint_free(matrix_unsignedint * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_float_free(matrix_float * obj) {
+    void matrix_float_free(matrix_float * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void matrix_double_free(matrix_double * obj) {
+    void matrix_double_free(matrix_double * obj) {
 
-		unsigned int iRow;
+        unsigned int iRow;
 
-		for (iRow = 0; iRow < obj->nRows; iRow++) {
-			free((void *) obj->array[iRow]);
-		}
-		free((void *) obj->array);
-		free((void *) obj);
+        for (iRow = 0; iRow < obj->nRows; iRow++) {
+            free((void *) obj->array[iRow]);
+        }
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
--- src/signal/vector.c
+++ src/signal/vector.c
@@ -1,162 +1,162 @@
-	
-	#include "vector.h"
+    
+    #include "vector.h"
 
-	vector_signedchar * vector_signedchar_malloc(const unsigned int nElements) {
+    vector_signedchar * vector_signedchar_malloc(const unsigned int nElements) {
 
-		vector_signedchar * obj;
+        vector_signedchar * obj;
 
-		obj = (vector_signedchar *) malloc(sizeof(vector_signedchar));
+        obj = (vector_signedchar *) malloc(sizeof(vector_signedchar));
 
-		obj->array = (signed char *) malloc(sizeof(signed char) * nElements);
-		obj->nElements = nElements;
+        obj->array = (signed char *) malloc(sizeof(signed char) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
-	
-	vector_unsignedchar * vector_unsignedchar_malloc(const unsigned int nElements) {
+    }
+    
+    vector_unsignedchar * vector_unsignedchar_malloc(const unsigned int nElements) {
 
-		vector_unsignedchar * obj;
+        vector_unsignedchar * obj;
 
-		obj = (vector_unsignedchar *) malloc(sizeof(vector_unsignedchar));
+        obj = (vector_unsignedchar *) malloc(sizeof(vector_unsignedchar));
 
-		obj->array = (unsigned char *) malloc(sizeof(unsigned char) * nElements);
-		obj->nElements = nElements;
+        obj->array = (unsigned char *) malloc(sizeof(unsigned char) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	vector_signedshort * vector_signedshort_malloc(const unsigned int nElements) {
+    vector_signedshort * vector_signedshort_malloc(const unsigned int nElements) {
 
-		vector_signedshort * obj;
+        vector_signedshort * obj;
 
-		obj = (vector_signedshort *) malloc(sizeof(vector_signedshort));
+        obj = (vector_signedshort *) malloc(sizeof(vector_signedshort));
 
-		obj->array = (signed short *) malloc(sizeof(signed short) * nElements);
-		obj->nElements = nElements;
+        obj->array = (signed short *) malloc(sizeof(signed short) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	vector_unsignedshort * vector_unsignedshort_malloc(const unsigned int nElements) {
+    vector_unsignedshort * vector_unsignedshort_malloc(const unsigned int nElements) {
 
-		vector_unsignedshort * obj;
+        vector_unsignedshort * obj;
 
-		obj = (vector_unsignedshort *) malloc(sizeof(vector_unsignedshort));
+        obj = (vector_unsignedshort *) malloc(sizeof(vector_unsignedshort));
 
-		obj->array = (unsigned short *) malloc(sizeof(unsigned short) * nElements);
-		obj->nElements = nElements;
+        obj->array = (unsigned short *) malloc(sizeof(unsigned short) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	vector_signedint * vector_signedint_malloc(const unsigned int nElements) {
+    vector_signedint * vector_signedint_malloc(const unsigned int nElements) {
 
-		vector_signedint * obj;
+        vector_signedint * obj;
 
-		obj = (vector_signedint *) malloc(sizeof(vector_signedint));
+        obj = (vector_signedint *) malloc(sizeof(vector_signedint));
 
-		obj->array = (signed int *) malloc(sizeof(signed int) * nElements);
-		obj->nElements = nElements;
+        obj->array = (signed int *) malloc(sizeof(signed int) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
-	
-	vector_unsignedint * vector_unsignedint_malloc(const unsigned int nElements) {
+    }
+    
+    vector_unsignedint * vector_unsignedint_malloc(const unsigned int nElements) {
 
-		vector_unsignedint * obj;
+        vector_unsignedint * obj;
 
-		obj = (vector_unsignedint *) malloc(sizeof(vector_unsignedint));
+        obj = (vector_unsignedint *) malloc(sizeof(vector_unsignedint));
 
-		obj->array = (unsigned int *) malloc(sizeof(unsigned int) * nElements);
-		obj->nElements = nElements;
+        obj->array = (unsigned int *) malloc(sizeof(unsigned int) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	vector_float * vector_float_malloc(const unsigned int nElements) {
+    vector_float * vector_float_malloc(const unsigned int nElements) {
 
-		vector_float * obj;
+        vector_float * obj;
 
-		obj = (vector_float *) malloc(sizeof(vector_float));
+        obj = (vector_float *) malloc(sizeof(vector_float));
 
-		obj->array = (float *) malloc(sizeof(float) * nElements);
-		obj->nElements = nElements;
+        obj->array = (float *) malloc(sizeof(float) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	vector_double * vector_double_malloc(const unsigned int nElements) {
+    vector_double * vector_double_malloc(const unsigned int nElements) {
 
-		vector_double * obj;
+        vector_double * obj;
 
-		obj = (vector_double *) malloc(sizeof(vector_double));
+        obj = (vector_double *) malloc(sizeof(vector_double));
 
-		obj->array = (double *) malloc(sizeof(double) * nElements);
-		obj->nElements = nElements;
+        obj->array = (double *) malloc(sizeof(double) * nElements);
+        obj->nElements = nElements;
 
-		return obj;
+        return obj;
 
-	}
+    }
 
-	void vector_signedchar_free(vector_signedchar * obj) {
+    void vector_signedchar_free(vector_signedchar * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_unsignedchar_free(vector_unsignedchar * obj) {
+    void vector_unsignedchar_free(vector_unsignedchar * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_signedshort_free(vector_signedshort * obj) {
+    void vector_signedshort_free(vector_signedshort * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_unsignedshort_free(vector_unsignedshort * obj) {
+    void vector_unsignedshort_free(vector_unsignedshort * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_signedint_free(vector_signedint * obj) {
+    void vector_signedint_free(vector_signedint * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_unsignedint_free(vector_unsignedint * obj){
+    void vector_unsignedint_free(vector_unsignedint * obj){
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_float_free(vector_float * obj) {
+    void vector_float_free(vector_float * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
 
-	void vector_double_free(vector_double * obj) {
+    void vector_double_free(vector_double * obj) {
 
-		free((void *) obj->array);
-		free((void *) obj);
+        free((void *) obj->array);
+        free((void *) obj);
 
-	}
+    }
