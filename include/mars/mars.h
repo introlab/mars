a9289274749f025e17a040ab1190eddf5f518a53
FrancoisGrondin <francois.grondin2@usherbrooke.ca>
2016-11-01 14:45:43 -0400

    CMake updated

--- include/mars/mars.h
+++ include/mars/mars.h
@@ -1,1 +1,22 @@
+#include "../../src/signal/matrix.h"
+#include "../../src/signal/vector.h"
+
+#include "../../src/sink/hop2raw.h"
+
+#include "../../src/source/eightsnd2hop.h"
+#include "../../src/source/raw2hop.h"
+
+#include "../../src/system/frame2freq.h"
+#include "../../src/system/freq2mcra.h"
+#include "../../src/system/freq2phase.h"
+#include "../../src/system/freq2softmask.h"
+#include "../../src/system/hop2frame.h"
+#include "../../src/system/phase2phases.h"
+#include "../../src/system/phases2xcorr.h"
+#include "../../src/system/xcorr2tdoa.h"
+
 #include "../../src/utils/fft.h"
+#include "../../src/utils/fifo.h"
+#include "../../src/utils/indexing.h"
+#include "../../src/utils/pcm.h"
+#include "../../src/utils/window.h"
