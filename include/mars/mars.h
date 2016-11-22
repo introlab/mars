afaf3ace7384cf49bc1d1b52dee56d8c6758bc01
FrancoisGrondin <francois.grondin2@usherbrooke.ca>
2016-11-22 12:26:20 -0500

    Include file updated.

--- include/mars/mars.h
+++ include/mars/mars.h
@@ -1,22 +1,33 @@
+#include "../../src/general/circle.h"
+#include "../../src/general/map.h"
+#include "../../src/general/sphere.h"
+#include "../../src/general/tdoa.h"
+#include "../../src/general/window.h"
+
+#include "../../src/message/msg_hops.h"
+#include "../../src/message/msg_spectra.h"
+#include "../../src/message/msg_pots.h"
+
+#include "../../src/module/stft.h"
+#include "../../src/module/ssl.h"
+
+#include "../../src/probe/snapshot.h"
+
 #include "../../src/signal/matrix.h"
 #include "../../src/signal/vector.h"
 
-#include "../../src/sink/hop2raw.h"
+#include "../../src/sink/snk_raw.h"
+#include "../../src/sink/snk_pots.h"
 
-#include "../../src/source/eightsnd2hop.h"
-#include "../../src/source/raw2hop.h"
+#include "../../src/source/src_eightsnd.h"
+#include "../../src/source/src_raw.h"
 
-#include "../../src/system/frame2freq.h"
-#include "../../src/system/freq2mcra.h"
-#include "../../src/system/freq2phase.h"
-#include "../../src/system/freq2softmask.h"
-#include "../../src/system/hop2frame.h"
-#include "../../src/system/phase2phases.h"
-#include "../../src/system/phases2xcorr.h"
-#include "../../src/system/xcorr2tdoa.h"
-
+#include "../../src/utils/array.h"
 #include "../../src/utils/fft.h"
 #include "../../src/utils/fifo.h"
 #include "../../src/utils/indexing.h"
+#include "../../src/utils/minmax.h"
+#include "../../src/utils/offfcn.h"
+#include "../../src/utils/pair.h"
 #include "../../src/utils/pcm.h"
-#include "../../src/utils/window.h"
+
