30338e60b7a190f786a3fa3c7ff6da2fb988e118
FrancoisGrondin <francois.grondin2@usherbrooke.ca>
2017-01-29 18:10:43 -0500

    Include added.

---
+++ include/mars/mars.h
@@ -0,0 +1,16 @@
+
+    #include "../../src/message/msg_hops.h"
+    #include "../../src/message/msg_pots.h"
+    #include "../../src/message/msg_spectra.h"
+    #include "../../src/message/msg_tracks.h"
+
+    #include "../../src/module/mod_stft.h"
+    #include "../../src/module/mod_ssl.h"
+    #include "../../src/module/mod_sst.h"
+
+    #include "../../src/sink/snk_pots.h"
+    #include "../../src/sink/snk_tracks.h"
+
+    #include "../../src/source/src_raw.h"
+
+    #include "../../src/utils/random.h"
